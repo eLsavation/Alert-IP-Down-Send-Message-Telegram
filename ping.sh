4ced07c5d8cb4184981cbba34203c2d986258aed
eLsavation <51361409+eLsavation@users.noreply.github.com>
2020-09-08 18:27:13 +0700

    Update ping.sh

--- ping.sh
+++ ping.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
-# Program name: pingall.sh
-date
-cat list.txt |  while read output
-do
-    ping -c 1 "$output" > /dev/null
-    if [ $? -eq 0 ]; then
-    echo "node $output is up" 
-    else
-    echo "node $output is down"
-    fi
-done
+
+#!/bin/bash
+teleg() {
+        message="Alert Device Down%0A$1 Down Please Check%0ADate : `date +%Y-%m-%d`%0ATime : `date +%H:%M:%S`"
+        curl -skL "https://api.telegram.org/bot666:api/sendMessage?chat_id=-666&text=$message"
+}
+while IFS= read -r ip; do #open file and store to variable ip
+
+        ping -c 2 $ip &> /dev/null
+        if [ $? -ne 0 ]; then
+                echo "$ip is DOWN!"
+                teleg $ip
+        else
+                echo "$ip is UP!"
+        fi
+
+#current_date_time="`date`";
+#echo $current_date_time;
+done </home/eLsavation/telebot/ip.txt
+
